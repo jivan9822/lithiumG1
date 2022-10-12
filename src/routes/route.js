cf82851cd2738dcd11de29fbc828047882d83247
jivan9822 <j.toshniwal@yahoo.co.in>
2022-10-12 23:15:07 +0530

    i make router function shorter

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,5 +1,4 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
 
 router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
