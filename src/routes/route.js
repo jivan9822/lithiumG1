9b3b66273031e1240a01498611edfa9ec65c713f
jivan9822 <j.toshniwal@yahoo.co.in>
2022-10-17 15:31:20 +0530

    Practice on Student api

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
-
+const intro = require('./introduction/intro');
+const intro2 = require('../introduction/intro');
 router.get('/test-me', function (req, res) {
     console.log('My batch is', abc.name)
     abc.printName()
