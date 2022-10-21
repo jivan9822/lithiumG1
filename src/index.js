89393247263443e35bc75dfb2d7e0880a2663665
jivan9822 <j.toshniwal@yahoo.co.in>
2022-10-21 22:21:18 +0530

    Trial of code sucuessfull

--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const route = require('./routes/route.js');
-const { default: mongoose } = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const route = require("./routes/route.js");
+const { default: mongoose } = require("mongoose");
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
-    useNewUrlParser: true
-})
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
-
-app.use('/', route);
-
+mongoose.connect(
+  "mongodb+srv://j_jivan:223521@cluster0.tbpicad.mongodb.net/UserInfo"
+);
+app.use("/", route);
 
 app.listen(process.env.PORT || 3000, function () {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+  console.log("Express app running on port " + (process.env.PORT || 3000));
 });
