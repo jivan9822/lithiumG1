9b826bc5d8689633996a2af93323afb9602b9030
jivan9822 <j.toshniwal@yahoo.co.in>
2022-10-21 15:36:34 +0530

    Create mongoose data base

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const route = require('./routes/route.js');
+const express = require("express");
+const bodyParser = require("body-parser");
+const route = require("./routes/route.js");
+// const { default: mongoose } = require("mongoose");
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/', route);
+app.use("/", route);
 
-app.listen(process.env.PORT || 3000, function() {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://j_jivan:223521@cluster0.tbpicad.mongodb.net/newFriut"
+);
+
+const fruitSchema = {
+  name: String,
+}
+
+const Fruit = mongoose.model("friut", fruitSchema);
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Express app running on port " + (process.env.PORT || 3000));
 });
