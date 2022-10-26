e3a5ccc11e8828d616507a59a6527af80c4f3ff6
jivan9822 <j.toshniwal@yahoo.co.in>
2022-10-26 11:19:29 +0530

    add books to data-bace complate

--- src/controllers/bookController2.js
+++ src/controllers/bookController2.js
@@ -1,8 +1,48 @@
 const bookStore = require("../models/bookModel2");
 
+// Added from Postman using ready api
+
+// createBook : to create a new entry..use this api to create 11+ entries in your collection
 const addBook2 = async (req, res) => {
   const bookCreate = await bookStore.create(req.body);
   res.send(bookCreate);
 };
+
+// Updated some field price, stockAvailable and tags 
+const price = () => Math.round(Math.random() * 1000);
+
+const updateDate = (req, res) => {
+  bookStore.find({}, (err, found) => {
+    found.map((each,index) => {
+      const temp = price();
+      each.price = {
+        Indian: `${temp}INR`,
+        European: `${(temp / 82).toFixed(2)}EURO`,
+      };
+      if (index % 7 === 0) {
+        each.stockAvilable = false;
+      } else {
+        each.stockAvilable = true;
+      }
+      each.tags.push("No Tag Avaliable");
+      each.save();
+    });
+    console.log(found);
+  });
+};
+// updateDate();
+
+// All Queries
+
+// 1) bookList : gives all the books- their bookName and authorName only
+// getBooksInYear: takes year as input in post request and gives list of all books published that year
+// getParticularBooks:- (this is a good one, make sincere effort to solve this) take any input and use it as a condition to fetch books that satisfy that condition
+// e.g if body had { name: “hi”} then you would fetch the books with this name
+// if body had { year: 2020} then you would fetch the books in this year
+// hence the condition will differ based on what you input in the request body
+// getXINRBooks- request to return all books who have an Indian price tag of “100INR” or “200INR” or “500INR”
+// getRandomBooks - returns books that are available in stock or have more than 500 pages
+
+
 
 module.exports.addBook2 = addBook2;
--- src/models/bookModel2.js
+++ src/models/bookModel2.js
@@ -3,6 +3,14 @@
 
 const mongoose = require("mongoose");
 
+  // "author": "Dante Alighieri",
+  // "country": "Italy",
+  // "imageLink": "images/the-divine-comedy.jpg",
+  // "language": "Italian",
+  // "link": "https://en.wikipedia.org/wiki/Divine_Comedy\n",
+  // "pages": 928,
+  // "year": 1315,
+
 const bookSchema = new mongoose.Schema(
   {
     author: String,
@@ -30,14 +38,3 @@ const bookSchema = new mongoose.Schema(
 );
 
 module.exports = mongoose.model("Book2", bookSchema);
-
-// create the following API’s (write logic in bookController and routes in routes):
-// createBook : to create a new entry..use this api to create 11+ entries in your collection
-// bookList : gives all the books- their bookName and authorName only
-// getBooksInYear: takes year as input in post request and gives list of all books published that year
-// getParticularBooks:- (this is a good one, make sincere effort to solve this) take any input and use it as a condition to fetch books that satisfy that condition
-// e.g if body had { name: “hi”} then you would fetch the books with this name
-// if body had { year: 2020} then you would fetch the books in this year
-// hence the condition will differ based on what you input in the request body
-// getXINRBooks- request to return all books who have an Indian price tag of “100INR” or “200INR” or “500INR”
-// getRandomBooks - returns books that are available in stock or have more than 500 pages
